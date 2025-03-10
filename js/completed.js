8b70194114126f7e7ac42276d87190862097c515
ahasanshaker <ahasanullalshaker@gmail.com>
2025-03-10 22:36:50 +0600

    updated button and javascript

---
+++ js/completed.js
@@ -0,0 +1,34 @@
+
+const totalButtons = 6;
+let completedButtons = 0; 
+document.querySelectorAll('.btn-completed').forEach(button =>{
+    button.addEventListener('click',function(){
+        const taskAssignedElement = document.getElementById('task-assigned');
+        let taskAssigned = parseInt(taskAssignedElement.innerText);
+        if(taskAssigned >0){
+        taskAssignedElement.innerText = taskAssigned-1;
+    }
+        const checkboxPraElement = document.getElementById('checkbox-pra');
+        let checkboxPra = parseInt(checkboxPraElement.innerText);
+        checkboxPraElement.innerText = checkboxPra + 1;
+
+        this.disabled = true;
+        this.style.backgroundColor = '#ccc'; 
+        this.style.color = '#666'; 
+        this.style.cursor = "not-allowed";
+        completedButtons++;
+        if(completedButtons === totalButtons){
+            const currenttime = new Date().toLocaleString();
+            const completedMsg =`You have Complete The Task Add Dark Mode at ${currenttime}`;
+            const historyy =document.getElementById('history');
+            const p= document.createElement('p');
+            p.innerText= completedMsg;
+            historyy.appendChild(p);
+        }
+       
+    })
+})
+document.getElementById('clear-history').addEventListener('click', function () {
+    const historyy  = document.getElementById('history');
+    historyy.innerHTML = ''; 
+})
